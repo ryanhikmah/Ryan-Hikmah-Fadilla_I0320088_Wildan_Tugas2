5c084471d0b657a2af141673aa34408eefc1a724
ryanhikmah <80144820+ryanhikmah@users.noreply.github.com>
2021-03-12 16:23:17 -0800

    penambahan biodata

--- I0320088_soal2_tugas2.py
+++ I0320088_soal2_tugas2.py
@@ -3,6 +3,22 @@ nama_panjang = "Ryan Hikmah Fadilla"
 print("nama panjang: ",nama_panjang)
 nama_panggilan = "Ryan"
 print("nama panggilan: ",nama_panggilan)
+tempat_lahir = "Surakarta"
+print("Tempat Lahir: ",tempat_lahir)
+tanggal_lahir = 23
+bulan_lahir = 6
+tahun_lahir = 2002
+print("Tanggal Lahir: ",tanggal_lahir, "Bulan Lahir: ",bulan_lahir, "Tahun Lahir: ",tahun_lahir)
+asal_daerah = "Surakarta"
+print("Asal Daerah: ",asal_daerah)
+nama_ayah = "Andhi Ariyanto"
+print("Nama Ayah: ",nama_ayah)
+nama_ibu = "Catur Sari Kahayanti"
+print("Nama Ibu: ",nama_ibu)
+jumlah_saudara = 1
+print("Jumlah Saudara Kandung: ",jumlah_saudara)
+nama_adik = "Luthfi Khairul Ikhwan"
+print("Nama Adik: ",nama_adik)
 tinggi_badan = 160
 print("tinggi badan: ",tinggi_badan)
 berat_badan = 55
@@ -18,19 +34,32 @@ print("jenis kelamin: ",jenis_kelamin)
 golongan_darah = "O"
 print("Golongan Darah: ",golongan_darah)
 alamat_rumah = "Perumahan Griya Sentosa 2 Blok B"
-alamat_NoRumah = 2
+alamat_NoRumah ="No.2"
 alamat_kota= "Surakarta, Jawa Tengah"
-print("Alamat saya adalah ", alamat_rumah, "No. ", alamat_NoRumah, "Kota", alamat_kota)
+kode_pos = 57127
+print("Saya tinggal di: ",alamat_rumah, alamat_NoRumah, "Kode Pos: ",kode_pos)
 hobi = "Sepak Bola"
 print("Hobi: ",hobi)
+makanan_favorit = "Nasi Goreng"
+print("Makanan Favorit: ",makanan_favorit)
+minuman_favorit = "Es Kopi Susu"
+print("Minuman Favorit: ", minuman_favorit)
 anak_ke = 1
 print("anak ke: ",anak_ke)
 cita_cita = "pengusaha"
 print("Cita-cita: ",cita_cita)
+pekerjaan = "mahasiswa"
+print("pekerjaan: ", pekerjaan)
 universitas = "Universitas Sebelas Maret"
 print("Saya berkuliah di ",universitas)
-program_studi = "Teknik Industri"
+program_studi = "S-1 Teknik Industri"
 print("Program Studi: ",program_studi)
+jalur_masuk = "SBMPTN"
+print("Jalur Masuk: ",jalur_masuk)
+alamat_email = "ryan.hikmah.fadilla@gmail.com"
+print("Alamat Email: ",alamat_email)
+nama_instagram = "@ryanhikmah"
+print("Nama Instagram: ",nama_instagram)
 
 
 
